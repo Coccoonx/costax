fcda873ca4e77265735cb017261c58ed270d889d
Lyonnel Dzotang <dtlyonnel@gmail.com>
2017-01-26 02:23:48 +0100

    adding map component

--- app/src/main/java/com/mobilesoft/bonways/uis/fragments/MapFragment.java
+++ app/src/main/java/com/mobilesoft/bonways/uis/fragments/MapFragment.java
@@ -1,11 +1,26 @@
 package com.mobilesoft.bonways.uis.fragments;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.mobilesoft.bonways.R;
 
 /**
@@ -14,9 +29,44 @@ import com.mobilesoft.bonways.R;
 
 public class MapFragment extends Fragment {
 
+    GoogleMap googleMap;
+    private MapView mapView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mapView = (MapView) v.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        if (mapView != null) {
+        }
+
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+       mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+       mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 
 }
